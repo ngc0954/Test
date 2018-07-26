831073fea4769468d595afdaf74b751fc980efcb
gopichand <chandu.gopi2010@gmail.com>
2018-07-26 20:51:36 +0530

    added test in server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/test'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/test/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
